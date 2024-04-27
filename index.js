035abf08fd8cd9dc7b3ed3e8186f6f68bf8aa4f9
shohan15020 <mdshohansajjad@gmail.com>
2024-04-27 13:23:18 +0600

    update done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const  cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -33,19 +33,78 @@ async function run() {
     const tourCollection = client.db("TourismDB").collection("tour");
 
     // create a post
-    app.post('/allTour', async(req, res)=>{
+    app.post('/allTour', async (req, res) => {
       const cursor = req.body
       const result = await tourCollection.insertOne(cursor)
       res.send(result)
     })
-    // read by email
-    app.get('/allTour/:email', async(req, res) => {
-      const email = req.params.email
-      console.log(email)
-      const query = { email: email };
-      const result = await tourCollection.find(query).toArray();
+    // read all
+    app.get('/allTour', async (req, res) => {
+      const cursor = tourCollection.find()
+      const result = await cursor.toArray()
       res.send(result)
     })
+    // read a item
+    app.get('/views/:id', async (req, res) => {
+      const id = req.params.id
+      const cursor = { _id: new ObjectId(id) }
+      const result = await tourCollection.findOne(cursor);
+      res.send(result)
+    })
+    // // read by email
+    app.get('/myList/:email', async (req, res) => {
+      const email = req.params.email
+      console.log(email)
+      const query = { email: (email) };
+      const result = await tourCollection.find(query).toArray();
+      // console.log(result)
+      res.send(result)
+    })
+    // delete
+    app.delete('/delete/:id', async (req, res) => {
+      const id = req.params.id
+
+      const query = { _id: new ObjectId(id) };
+
+
+      const result = await tourCollection.deleteOne(query);
+      // console.log(result)
+      res.send(result)
+    })
+    // update
+    app.get('/update/:id', async (req, res) => {
+      const id = req.params.id
+      const cursor = { _id: new ObjectId(id) }
+      const result = await tourCollection.findOne(cursor);
+      res.send(result)
+    })
+    app.put('/update/:id', async (req, res) => {
+      const id = req.params.id
+      const filter = { _id: new ObjectId(id) };
+      const updateOne = req.body;
+      const options = { upsert: true };
+      // Specify the update to set a value for the plot field
+      const update = {
+        $set: {
+          // name:updateOne.name,
+          // email:updateOne.email,
+          tourists_spot_name:updateOne.tourists_spot_name,
+          country_Name:updateOne.country_Name, 
+          location:updateOne.location, 
+          average_cost:updateOne.average_cost, 
+          totalVisitorsPerYear:updateOne.totalVisitorsPerYear, 
+          travel_time:updateOne.travel_time, 
+          seasonality:updateOne.seasonality, 
+          short_description:updateOne.short_description, 
+          image:updateOne.image,
+        },
+      };
+      // Update the first document that matches the filter
+      const result = await tourCollection.updateOne(filter, update, options);
+
+      res.send(result)
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
