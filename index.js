0a93757935091c6dcdfd893bbfd4a1404312b79e
shohan15020 <mdshohansajjad@gmail.com>
2024-04-28 21:13:04 +0600

    edited navbar

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+    
     const tourCollection = client.db("TourismDB").collection("tour");
     const countriesCollection = client.db("TourismDB").collection("countries");
     // console.log(countriesCollection)
